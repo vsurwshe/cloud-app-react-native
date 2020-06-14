fb2294c03bf783431ee8407f6928cf779e70f303
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-06-14 17:39:48 +0530

    0.0.3: Added the home app make like run expo app

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screen/Home'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app123!</Text>
+      <Home />
     </View>
   );
 }
